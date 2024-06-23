dc9f7dcded1c55c99ebf3d35771c3fe0a179d77f
gwynnbleiidd <xnovaua@gmail.com>
2024-06-23 08:24:03 +0000

    update plugins (b7ded71f4a7121214dbec9a0ec44b3b68ef8b41f)

---
+++ lmeQuality/lmeQuality.js
@@ -0,0 +1,68 @@
+(function () {
+    'use strict';
+
+    function card() {
+      Lampa.Listener.follow("full", function (e) {
+        if (e.type === "complite") {
+          $(document).ready(function () {
+            var imdbID = e.data.movie.imdb_id; // Замените на нужный ID
+            var apiUrl = "https://yts.mx/api/v2/movie_details.json?imdb_id=".concat(imdbID);
+            $.getJSON(apiUrl, function (data) {
+              var newDivider = $("<span class='full-start-new__split'>").html("\u25CF");
+              if (data.status === "ok") {
+                var movie = data.data.movie;
+                if (movie.id === 0) {
+                  var newSpan = $("<span>").html("".concat(Lampa.Lang.translate('player_quality'), ": Not found"));
+                  $(".full-start-new__details").append(newDivider, newSpan);
+                  return; // Прекращаем выполнение если id равен 0 <span>●</span>
+                }
+                var torrents = movie.torrents;
+                if (torrents.length > 0) {
+                  var lastTorrent = torrents[torrents.length - 1];
+                  var quality = lastTorrent.quality;
+                  // Преобразование форматов качества
+                  switch (quality) {
+                    case '720p':
+                      quality = 'HD';
+                      break;
+                    case '1080p':
+                      quality = 'Full HD';
+                      break;
+                    case '2160p':
+                      quality = '4K';
+                      break;
+                    default:
+                      quality = quality;
+                    // Оставляем как есть, если формат не распознан
+                  }
+                  var _newSpan = $("<span>").html("".concat(Lampa.Lang.translate('player_quality'), ": ").concat(quality));
+                  $(".full-start-new__details").append(newDivider, _newSpan);
+                }
+              } else {
+                console.error("Error fetching movie details:", data.status_message);
+              }
+            }).fail(function (jqXHR, textStatus, errorThrown) {
+              console.error("Fetch error:", textStatus, errorThrown);
+            });
+          });
+        }
+      });
+    }
+    var UTILS = {
+      card: card
+    };
+
+    function add() {
+      UTILS.card();
+    }
+    function startPlugin() {
+      window.plugin_lmeq_ready = true;
+      if (window.appready) add();else {
+        Lampa.Listener.follow("app", function (e) {
+          if (e.type === "ready") add();
+        });
+      }
+    }
+    if (!window.plugin_lmeq_ready) startPlugin();
+
+})();
