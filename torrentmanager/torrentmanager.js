4646163ab157dbf6711517c71990ffb442b50f36
gwynnbleiidd <xnovaua@gmail.com>
2024-08-04 17:19:20 +0000

    update plugins (e20d1fcbc1f2536057a8d7dc2b7c6465bf07d152)

--- torrentmanager/torrentmanager.js
+++ torrentmanager/torrentmanager.js
@@ -148,6 +148,27 @@
         return a;
       }
     }
+    function ownKeys(e, r) {
+      var t = Object.keys(e);
+      if (Object.getOwnPropertySymbols) {
+        var o = Object.getOwnPropertySymbols(e);
+        r && (o = o.filter(function (r) {
+          return Object.getOwnPropertyDescriptor(e, r).enumerable;
+        })), t.push.apply(t, o);
+      }
+      return t;
+    }
+    function _objectSpread2(e) {
+      for (var r = 1; r < arguments.length; r++) {
+        var t = null != arguments[r] ? arguments[r] : {};
+        r % 2 ? ownKeys(Object(t), !0).forEach(function (r) {
+          _defineProperty(e, r, t[r]);
+        }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t)) : ownKeys(Object(t)).forEach(function (r) {
+          Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r));
+        });
+      }
+      return e;
+    }
     function _regeneratorRuntime() {
       _regeneratorRuntime = function () {
         return e;
@@ -449,6 +470,20 @@
         }
       }, e;
     }
+    function _toPrimitive(t, r) {
+      if ("object" != typeof t || !t) return t;
+      var e = t[Symbol.toPrimitive];
+      if (void 0 !== e) {
+        var i = e.call(t, r || "default");
+        if ("object" != typeof i) return i;
+        throw new TypeError("@@toPrimitive must return a primitive value.");
+      }
+      return ("string" === r ? String : Number)(t);
+    }
+    function _toPropertyKey(t) {
+      var i = _toPrimitive(t, "string");
+      return "symbol" == typeof i ? i : i + "";
+    }
     function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {
       try {
         var info = gen[key](arg);
@@ -478,6 +513,20 @@
           _next(undefined);
         });
       };
+    }
+    function _defineProperty(obj, key, value) {
+      key = _toPropertyKey(key);
+      if (key in obj) {
+        Object.defineProperty(obj, key, {
+          value: value,
+          enumerable: true,
+          configurable: true,
+          writable: true
+        });
+      } else {
+        obj[key] = value;
+      }
+      return obj;
     }
     function _slicedToArray(arr, i) {
       return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest();
@@ -785,13 +834,15 @@
         method: "POST",
         timeout: 0,
         headers: getHeaders$1(),
-        "data": JSON.stringify({
-          "method": "torrent-add",
-          "arguments": {
-            "paused": Lampa.Storage.field("lmetorrenttransmissionAutostart") ? "true" : "false",
-            "download-dir": selectedTorrent.CategoryDesc ? Lampa.Storage.get("lmetorrenttransmission".concat(selectedTorrent.CategoryDesc)) : '',
-            "filename": selectedTorrent.MagnetUri ? selectedTorrent.MagnetUri : selectedTorrent.Link
-          }
+        data: JSON.stringify({
+          method: "torrent-add",
+          arguments: _objectSpread2({
+            paused: Lampa.Storage.field("lmetorrenttransmissionAutostart") ? "true" : "false",
+            sequentialDownload: Lampa.Storage.field("lmetorrenttransmissionSequentialDownload") ? "true" : "false",
+            filename: selectedTorrent.MagnetUri ? selectedTorrent.MagnetUri : selectedTorrent.Link
+          }, Lampa.Storage.get("lmetorrenttransmission".concat(selectedTorrent.CategoryDesc)) ? {
+            "download-dir": Lampa.Storage.get("lmetorrenttransmission".concat(selectedTorrent.CategoryDesc))
+          } : {})
         })
       };
       return new Promise(function (resolve, reject) {
@@ -1524,6 +1575,26 @@
         },
         field: {
           name: 'Autostop'
+        },
+        onRender: function onRender(item) {
+          if (Lampa.Storage.field(manifest.component + 'Select') === "transmission" && Lampa.Storage.field(manifest.component + "transmissionTweak") === true) {
+            item.show();
+          } else item.hide();
+        },
+        onChange: function onChange(value) {
+          Lampa.Settings.update();
+        }
+      });
+      Lampa.SettingsApi.addParam({
+        component: manifest.component,
+        param: {
+          name: manifest.component + "transmissionSequentialDownload",
+          type: "trigger",
+          //доступно select,input,trigger,title,static
+          "default": false
+        },
+        field: {
+          name: 'SequentialDownload'
         },
         onRender: function onRender(item) {
           if (Lampa.Storage.field(manifest.component + 'Select') === "transmission" && Lampa.Storage.field(manifest.component + "transmissionTweak") === true) {
