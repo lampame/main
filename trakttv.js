30418dd27c8bfad79ebe8e95a0e208ceac21f9ec
Felix <xnovaua@gmail.com>
2025-06-07 09:22:45 +0300

    Update trakttv.js

--- trakttv.js
+++ trakttv.js
@@ -1,6 +1,12 @@
 (function () {
     'use strict';
-
-    Lampa.Utils.putScriptAsync(['https://lampame.github.io/main/TraktTV/TraktTV.js'], function () { });
-
+      try {
+        // Перевірка синтаксису ES6 (default params, arrow functions, classes)
+        new Function("(a = 0) => { class Test {} }")();
+        Lampa.Utils.putScriptAsync(['https://lampame.github.io/main/TraktTV/TraktTV.js'], function () { });
+        console.log('LME', 'TraktTV ES6');
+      } catch (e) {
+        Lampa.Utils.putScriptAsync(['https://lampame.github.io/main/es5/TraktTV/TraktTV.js'], function () { });
+          console.log('LME', 'TraktTV ES5');
+      }
 })();
