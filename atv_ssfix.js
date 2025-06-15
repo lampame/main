0bff778c2bf85859728b2d3bbe99ccfb5747d414
Felix <xnovaua@gmail.com>
2025-06-15 17:08:16 +0300

    Update atv_ssfix.js

--- atv_ssfix.js
+++ atv_ssfix.js
@@ -1,24 +1,30 @@
-Lampa.Platform.tv();
-function add() {
-    if (Lampa.Platform.is('apple_tv') == true) {
-      Lampa.Storage.set("screensaver", false)
-    }
-}
-function startPlugin() {
-    window.plugin_atvss_ready = true;
-
-    Lampa.Manifest.plugins = {
-        type: "other",
-        version: "0.3",
-        name: "AppleTV Fix",
-        description: "Fix screensaver",
-        component: "atvss",
-    };
-    if (window.appready) add();
-    else {
-        Lampa.Listener.follow("app", function (e) {
-            if (e.type === "ready") add();
-        });
-    }
-}
+function add() {  
+    Lampa.Storage.listener.follow('change', (event) => {  
+        if (event.name == 'screensaver' && event.value == true) {  
+            if (Lampa.Platform.is('apple_tv') == true) {  
+                Lampa.Storage.set("screensaver", false)  
+            }  
+        }  
+    })  
+}  
+  
+function startPlugin() {  
+    window.plugin_atvss_ready = true;  
+  
+    Lampa.Manifest.plugins = {  
+        type: "other",  
+        version: "0.3",  
+        name: "AppleTV Fix",  
+        description: "Fix screensaver",  
+        component: "atvss",  
+    };       
+  
+    if (window.appready) add();  
+    else {  
+        Lampa.Listener.follow("app", function (e) {  
+            if (e.type === "ready") add();  
+        });  
+    }  
+}  
+  
 if (!window.plugin_atvss_ready) startPlugin();
