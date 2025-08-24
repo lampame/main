f3b288dad816990781af35553204c9fd64aec11f
Felix <midi2001@mail.ru>
2025-08-24 10:12:00 +0300

    Increase maxBuffer for child process exec to prevent stdout overflow

--- devtd.js
+++
@@ -1,7 +0,0 @@
-(function () {
-	'use strict';
-
-	var hash = Lampa.Utils.hash(111);
-	Lampa.Utils.putScriptAsync(['https://lampame.github.io/main/td/td.js'], function () { });
-	//[addPluginParams('https://lampame.github.io/td/td/td.js')]
-})();
--- torrstream.js
+++
@@ -1,6 +0,0 @@
-(function () {
-    'use strict';
-
-    Lampa.Utils.putScriptAsync(['https://lampame.github.io/main/torrenstream/torrenstream.js'], function () { });
-
-})();
